2dd55c83a4df9d0346d891ce44694b4be904ad98
madhesh-maasi <madheshmaasi3@gmail.com>
2023-05-09 18:48:48 +0530

    First Push

---
+++ src/webparts/nonBillingReviewForm/components/INonBillingReviewFormProps.ts
@@ -0,0 +1,7 @@
+export interface INonBillingReviewFormProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/nonBillingReviewForm/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface INonBillingReviewFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'NonBillingReviewFormWebPartStrings' {
+  const strings: INonBillingReviewFormWebPartStrings;
+  export = strings;
+}
